85a92e1e6144f4edd5738bb9b19ba36de5bd915d
Rei  Matsuda <rei.matsuda@imjp.co.jp>
2019-07-11 17:39:31 +0900

    現状一旦保存

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,9 @@
 import camera from './modules/cameraInCanvas';
+import editing from './modules/canvasEditing';
 class App {
   constructor() {
     new camera();
+    new editing();
   }
 }
 new App();
--- resources/js/modules/cameraInCanvas.js
+++ resources/js/modules/cameraInCanvas.js
@@ -1,38 +1,59 @@
 export default class {
   constructor() {
+    //要素取得
     this.$video = document.querySelector('.camera');
     this.$canvas = document.querySelector('.canvas');
     this.$captureButton = document.querySelector('.js-capture');
+    this.$scareUpButton = document.querySelector('.js-scaleUp');
+    this.$scareDownButton = document.querySelector('.js-scaleDown');
 
     this.context = this.$canvas.getContext('2d');
 
-    // this.context.fillRect(0, 0, 100, 100);
     this.startCamera();
     this.bind();
   }
+
   bind() {
-    //イベント登録
-    ////ボタン押した時にキャプチャ
-    //////バックのキャンバスに固定する
+    //画面キャプチャ
+    this.$captureButton.addEventListener('click', () => {
+      this.renderCameraImageInCanvas();
+    });
   }
 
-  // getDeviceStream() {
-  //   if (navigator.mediaDevices.contains('getUserMedia')) {
-  //     console.log('test');
-  //   }
-  // }
   startCamera() {
-    // this.getDeviceStream();
     const MEDIA = navigator.mediaDevices.getUserMedia({
-      video: { facingMode: 'environment' },
+      video: {
+        facingMode: 'environment',
+        width: 300,
+        height: 300
+      },
       audio: false
     });
 
     MEDIA.then(stream => {
       this.$video.srcObject = stream;
     }).catch(error => {
+      console.log(this.$video);
       alert(error);
     });
   }
-  renderInCanvas() {}
+  stopCamera() {
+    const TRACKS = this.$video.srcObject.getTracks();
+    TRACKS.forEach(function(track) {
+      track.stop();
+    });
+    this.$video.srcObject = null;
+  }
+  renderCameraImageInCanvas() {
+    const VIDEO_WIDTH = this.$video.offsetWidth;
+    const VIDEO_HEIGHT = this.$video.offsetHeight;
+    this.context.drawImage(this.$video, 0, 0, VIDEO_WIDTH, VIDEO_HEIGHT);
+
+    this.$video.classList.add('js-hide');
+    this.$captureButton.classList.add('js-hide');
+    this.$scareUpButton.classList.remove('js-hide');
+    this.$scareDownButton.classList.remove('js-hide');
+
+    this.stopCamera();
+  }
 }
