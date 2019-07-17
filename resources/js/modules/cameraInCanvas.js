6e22148b0226d3cb3594662b7cff64d712ce88be
Rei  Matsuda <rei.matsuda@imjp.co.jp>
2019-07-17 18:31:29 +0900

    拡大縮小、ドラッグ移動の機能実装（とりあえず）

--- resources/js/modules/cameraInCanvas.js
+++ resources/js/modules/cameraInCanvas.js
@@ -20,6 +20,18 @@ export default class {
     this.originalImage = null;
     this.imageWidth = 300;
     this.imageHeight = 300;
+    this.scale = 1;
+    // this.trimmingRate = 1;
+    this.imagePosition = { x: 0, y: 0 };
+    this.imagePosition_past = { x: 0, y: 0 };
+    this.pointerPosition = {
+      startX: 0,
+      startY: 0,
+      currentX: 0,
+      currentY: 0
+    };
+    this.diff = { x: 0, y: 0 };
+    this.moveImageFlag = false;
 
     this.startCamera();
     this.bind();
@@ -31,12 +43,26 @@ export default class {
       this.renderCameraImageInCanvas();
     });
 
+    this.$canvas.addEventListener('mousedown', event => {
+      this.handleMouseDown(event);
+    });
+
+    document.addEventListener('mousemove', event => {
+      this.handleMouseMove(event);
+    });
+
+    document.addEventListener('mouseup', event => {
+      this.handleMouseUp(event);
+    });
+
     //拡大縮小ボタン
-    this.$scaleUpButton.addEventListener('click', () => {
-      this.scaleUpRender();
+    this.$scaleUpButton.addEventListener('click', event => {
+      this.scale *= 1.1;
+      this.render();
     });
     this.$scaleDownButton.addEventListener('click', () => {
-      this.scaleDownRender();
+      this.scale *= 0.9;
+      this.render();
     });
   }
 
@@ -82,16 +108,63 @@ export default class {
     this.originalImage = this.context.getImageData(0, 0, 300, 300);
   }
 
-  scaleUpRender() {
-    this.context.clearRect(0, 0, 300, 300);
-    this.offScreenContext.putImageData(this.originalImage, 0, 0);
-    this.context.scale(1.2, 1.2);
-    this.context.drawImage(this.$offScreen, 0, 0, 300, 300);
+  handleMouseDown(event) {
+    this.moveImageFlag = true;
+    const START_X = event.screenX;
+    const START_Y = event.screenY;
+
+    this.pointerPosition.startX = START_X;
+    this.pointerPosition.startY = START_Y;
+    this.eventTarget = event.target;
+    this.imagePosition_past.x = this.imagePosition.x;
+    this.imagePosition_past.y = this.imagePosition.y;
   }
-  scaleDownRender() {
+
+  handleMouseMove(event) {
+    if (!this.moveImageFlag) return;
+    const CURRENT_X = event.screenX;
+    const CURRENT_Y = event.screenY;
+    this.pointerPosition.currentX = CURRENT_X;
+    this.pointerPosition.currentY = CURRENT_Y;
+    this.diff.x = CURRENT_X - this.pointerPosition.startX;
+    this.diff.y = CURRENT_Y - this.pointerPosition.startY;
+
+    const DISPLACEMENT_X = this.diff.x / this.scale;
+    const DISPLACEMENT_Y = this.diff.y / this.scale;
+
+    this.imagePosition.x = DISPLACEMENT_X + this.imagePosition_past.x;
+    this.imagePosition.y = DISPLACEMENT_Y + this.imagePosition_past.y;
+    this.render();
+  }
+
+  handleMouseUp(event) {
+    this.moveImageFlag = false;
+    this.pointerPosition.startX = 0;
+    this.pointerPosition.startY = 0;
+    this.pointerPosition.currentX = 0;
+    this.pointerPosition.currentY = 0;
+  }
+
+  render() {
     this.context.clearRect(0, 0, 300, 300);
     this.offScreenContext.putImageData(this.originalImage, 0, 0);
-    this.context.scale(0.8, 0.8);
-    this.context.drawImage(this.$offScreen, 0, 0, 300, 300);
+
+    const RENDER_POINT_X =
+      this.imagePosition.x * this.scale +
+      (this.imageWidth * (1 - this.scale)) / 2;
+    const RENDER_POINT_Y =
+      this.imagePosition.y * this.scale +
+      (this.imageHeight * (1 - this.scale)) / 2;
+    const RENDER_WIDTH = this.imageWidth * this.scale;
+    const RENDER_HEIGHT = this.imageHeight * this.scale;
+    this.context.drawImage(
+      this.$offScreen,
+      RENDER_POINT_X,
+      RENDER_POINT_Y,
+      RENDER_WIDTH,
+      RENDER_HEIGHT
+    );
+    // this.imagePosition.x = RENDER_POINT_X;
+    // this.imagePosition.y = RENDER_POINT_Y;
   }
 }
