0098f24d390e65ed6370c795c32bed2a4cd93577
Rei  Matsuda <rei.matsuda@imjp.co.jp>
2019-07-30 11:44:17 +0900

    一旦保存その２

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,43 +1,34 @@
 import Camera from './modules/cameraInCanvas';
 import Sticker from './modules/stickerInCanvas';
+import CanvasEditing from './modules/canvasEditing';
 class App {
   constructor() {
     this.camera = new Camera();
+    this.transration = new CanvasEditing();
     //キャンバス
     this.$canvas = document.querySelector('.canvas');
     this.context = this.$canvas.getContext('2d');
 
-    //オフスクリーン
-    this.$offScreen = document.createElement('canvas');
-    this.$offScreen.width = this.$canvas.width;
-    this.$offScreen.height = this.$canvas.height;
-    this.offScreenContext = this.$offScreen.getContext('2d');
-
-    //その他の要素取得
-    this.$scaleUpButton = document.querySelector('.js-scaleUp');
-    this.$scaleDownButton = document.querySelector('.js-scaleDown');
-    this.$scaleOkButton = document.querySelector('.js-scaleOk');
-
     this.phase = 1; //フェーズ番号1〜3
 
-    //変数等
+    // //変数等
     this.originalImage = null;
-    this.imageWidth = 300;
-    this.imageHeight = 300;
-    this.scale = 1;
-    this.scale_past = 1;
+    // this.imageWidth = 300;
+    // this.imageHeight = 300;
+    // this.scale = 1;
+    // this.scale_past = 1;
 
-    this.imagePosition = { x: 0, y: 0 };
-    this.imagePosition_past = { x: 0, y: 0 };
-    this.imageGeometricCenter = { x: 0, y: 0 };
-    this.pointerPosition = {
-      startX: 0,
-      startY: 0,
-      currentX: 0,
-      currentY: 0
-    };
-    this.diff = { x: 0, y: 0 };
-    this.isTouched = false;
+    // this.imagePosition = { x: 0, y: 0 };
+    // this.imagePosition_past = { x: 0, y: 0 };
+    // this.imageGeometricCenter = { x: 0, y: 0 };
+    // this.pointerPosition = {
+    //   startX: 0,
+    //   startY: 0,
+    //   currentX: 0,
+    //   currentY: 0
+    // };
+    // this.diff = { x: 0, y: 0 };
+    // this.isTouched = false;
 
     this.bind();
   }
@@ -46,49 +37,40 @@ class App {
     //画面キャプチャ
     this.camera.on('renderCameraImage', canvasData => {
       this.phase = 2;
-      this.$scaleUpButton.classList.remove('js-hide');
-      this.$scaleDownButton.classList.remove('js-hide');
-      this.$scaleOkButton.classList.remove('js-hide');
       this.originalImage = canvasData.getImageData(0, 0, 300, 300);
+      this.transration.init(this.originalImage);
     });
 
-    // this.$canvas.addEventListener('mousedown', event => {
-    //   this.handleMouseDown(event);
-    // });
-
-    // this.$canvas.addEventListener('mousemove', event => {
-    //   this.handleMouseMove(event);
-    // });
-
-    // document.addEventListener('mouseup', event => {
-    //   this.handleMouseUp(event);
-    // });
-
-    //拡大縮小ボタン
-    this.$scaleUpButton.addEventListener('click', event => {
-      this.scale_past = this.scale;
-      this.scale *= 1.1;
-      this.resize();
-    });
-    this.$scaleDownButton.addEventListener('click', () => {
-      this.scale_past = this.scale;
-      this.scale *= 0.9;
-      this.resize();
-    });
-    this.$scaleOkButton.addEventListener('click', () => {
-      this.$scaleUpButton.classList.add('js-hide');
-      this.$scaleDownButton.classList.add('js-hide');
-      this.$scaleOkButton.classList.add('js-hide');
-      // this.$stickerWrapper.classList.remove('js-hide');
-      // this.$removeStickerButton.classList.remove('js-hide');
-      // this.$createImageButton.classList.remove('js-hide');
-
-      this.originalImage = this.context.getImageData(0, 0, 300, 300);
-      // this.backImageScreenContext.putImageData(this.originalImage, 0, 0);
-
+    this.transration.on('requestedRenderSticker', canvasData => {
+      this.originalImage = canvasData.getImageData(0, 0, 300, 300);
       this.sticker = new Sticker(this.originalImage);
-      this.phase = 3;
     });
+
+    // //拡大縮小ボタン
+    // this.$scaleUpButton.addEventListener('click', event => {
+    //   this.scale_past = this.scale;
+    //   this.scale *= 1.1;
+    //   this.resize();
+    // });
+    // this.$scaleDownButton.addEventListener('click', () => {
+    //   this.scale_past = this.scale;
+    //   this.scale *= 0.9;
+    //   this.resize();
+    // });
+    // this.$scaleOkButton.addEventListener('click', () => {
+    //   this.$scaleUpButton.classList.add('js-hide');
+    //   this.$scaleDownButton.classList.add('js-hide');
+    //   this.$scaleOkButton.classList.add('js-hide');
+    //   // this.$stickerWrapper.classList.remove('js-hide');
+    //   // this.$removeStickerButton.classList.remove('js-hide');
+    //   // this.$createImageButton.classList.remove('js-hide');
+
+    //   this.originalImage = this.context.getImageData(0, 0, 300, 300);
+    //   // this.backImageScreenContext.putImageData(this.originalImage, 0, 0);
+
+    //   this.sticker = new Sticker(this.originalImage);
+    //   this.phase = 3;
+    // });
   }
 
   // handleMouseDown(event) {
@@ -139,54 +121,5 @@ class App {
   //   console.log(this.clickProperty);
   //   this.clickProperty = 0;
   // }
-
-  resize() {
-    this.context.clearRect(0, 0, 300, 300);
-    this.offScreenContext.putImageData(this.originalImage, 0, 0);
-
-    const RENDER_WIDTH = this.imageWidth * this.scale;
-    const RENDER_HEIGHT = this.imageHeight * this.scale;
-
-    this.imageGeometricCenter.x =
-      this.imagePosition.x + (this.imageWidth * this.scale_past) / 2;
-    this.imageGeometricCenter.y =
-      this.imagePosition.y + (this.imageHeight * this.scale_past) / 2;
-
-    const CENTER_DIFF_X = this.imageGeometricCenter.x - 150; //150: キャンバスサイズの半分
-    const CENTER_DIFF_Y = this.imageGeometricCenter.y - 150;
-
-    const RENDER_POINT_X =
-      (this.scale / this.scale_past) * CENTER_DIFF_X - RENDER_WIDTH / 2 + 150;
-
-    const RENDER_POINT_Y =
-      (this.scale / this.scale_past) * CENTER_DIFF_Y - RENDER_HEIGHT / 2 + 150;
-
-    this.context.drawImage(
-      this.$offScreen,
-      RENDER_POINT_X,
-      RENDER_POINT_Y,
-      RENDER_WIDTH,
-      RENDER_HEIGHT
-    );
-    this.imagePosition.x = RENDER_POINT_X;
-    this.imagePosition.y = RENDER_POINT_Y;
-  }
-
-  moveImage() {
-    this.context.clearRect(0, 0, 300, 300);
-    this.offScreenContext.putImageData(this.originalImage, 0, 0);
-
-    const RENDER_POINT_X = this.imagePosition.x;
-    const RENDER_POINT_Y = this.imagePosition.y;
-    const RENDER_WIDTH = this.imageWidth * this.scale;
-    const RENDER_HEIGHT = this.imageHeight * this.scale;
-    this.context.drawImage(
-      this.$offScreen,
-      RENDER_POINT_X,
-      RENDER_POINT_Y,
-      RENDER_WIDTH,
-      RENDER_HEIGHT
-    );
-  }
 }
 new App();
--- resources/js/modules/canvasEditing.js
+++ resources/js/modules/canvasEditing.js
@@ -1,3 +1,118 @@
-export default class {
-  constructor() {}
+import Events from 'events';
+export default class extends Events {
+  constructor() {
+    super();
+
+    //キャンバス
+    this.$canvas = document.querySelector('.canvas');
+    this.context = this.$canvas.getContext('2d');
+
+    //オフスクリーン
+    this.$offScreen = document.createElement('canvas');
+    this.$offScreen.width = this.$canvas.width;
+    this.$offScreen.height = this.$canvas.height;
+    this.offScreenContext = this.$offScreen.getContext('2d');
+    //その他の要素取得
+    this.$scaleUpButton = document.querySelector('.js-scaleUp');
+    this.$scaleDownButton = document.querySelector('.js-scaleDown');
+    this.$scaleOkButton = document.querySelector('.js-scaleOk');
+  }
+
+  init(backgroundImage) {
+    this.$scaleUpButton.classList.remove('js-hide');
+    this.$scaleDownButton.classList.remove('js-hide');
+    this.$scaleOkButton.classList.remove('js-hide');
+
+    //変数等
+    this.originalImage = backgroundImage;
+    this.imageWidth = 300;
+    this.imageHeight = 300;
+    this.scale = 1;
+    this.scale_past = 1;
+
+    this.imagePosition = { x: 0, y: 0 };
+    this.imagePosition_past = { x: 0, y: 0 };
+    this.imageGeometricCenter = { x: 0, y: 0 };
+    this.pointerPosition = {
+      startX: 0,
+      startY: 0,
+      currentX: 0,
+      currentY: 0
+    };
+    this.diff = { x: 0, y: 0 };
+    this.isTouched = false;
+
+    this.bind();
+  }
+
+  bind() {
+    //拡大縮小ボタン
+    this.$scaleUpButton.addEventListener('click', () => {
+      this.scale_past = this.scale;
+      this.scale *= 1.1;
+      this.resize();
+    });
+    this.$scaleDownButton.addEventListener('click', () => {
+      this.scale_past = this.scale;
+      this.scale *= 0.9;
+      this.resize();
+    });
+    this.$scaleOkButton.addEventListener('click', () => {
+      this.$scaleUpButton.classList.add('js-hide');
+      this.$scaleDownButton.classList.add('js-hide');
+      this.$scaleOkButton.classList.add('js-hide');
+
+      this.emit('requestedRenderSticker', this.context);
+      // this.phase = 3;
+    });
+  }
+
+  resize() {
+    this.context.clearRect(0, 0, 300, 300);
+    this.offScreenContext.putImageData(this.originalImage, 0, 0);
+
+    const RENDER_WIDTH = this.imageWidth * this.scale;
+    const RENDER_HEIGHT = this.imageHeight * this.scale;
+
+    this.imageGeometricCenter.x =
+      this.imagePosition.x + (this.imageWidth * this.scale_past) / 2;
+    this.imageGeometricCenter.y =
+      this.imagePosition.y + (this.imageHeight * this.scale_past) / 2;
+
+    const CENTER_DIFF_X = this.imageGeometricCenter.x - 150; //150: キャンバスサイズの半分
+    const CENTER_DIFF_Y = this.imageGeometricCenter.y - 150;
+
+    const RENDER_POINT_X =
+      (this.scale / this.scale_past) * CENTER_DIFF_X - RENDER_WIDTH / 2 + 150;
+
+    const RENDER_POINT_Y =
+      (this.scale / this.scale_past) * CENTER_DIFF_Y - RENDER_HEIGHT / 2 + 150;
+
+    this.context.drawImage(
+      this.$offScreen,
+      RENDER_POINT_X,
+      RENDER_POINT_Y,
+      RENDER_WIDTH,
+      RENDER_HEIGHT
+    );
+    this.imagePosition.x = RENDER_POINT_X;
+    this.imagePosition.y = RENDER_POINT_Y;
+  }
+
+  moveImage() {
+    this.context.clearRect(0, 0, 300, 300);
+    this.offScreenContext.putImageData(this.originalImage, 0, 0);
+
+    const RENDER_POINT_X = this.imagePosition.x;
+    const RENDER_POINT_Y = this.imagePosition.y;
+    const RENDER_WIDTH = this.imageWidth * this.scale;
+    const RENDER_HEIGHT = this.imageHeight * this.scale;
+    this.context.drawImage(
+      this.$offScreen,
+      RENDER_POINT_X,
+      RENDER_POINT_Y,
+      RENDER_WIDTH,
+      RENDER_HEIGHT
+    );
+  }
 }
